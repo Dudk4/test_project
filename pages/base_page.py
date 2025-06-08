71cdd26021de20e59474d18fea16c386ec385c82
Dudk4 <degtyarevkolya85@gmai.com>
2025-06-08 14:16:40 +0300

    first_test_with_pom

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,10 +1,18 @@
 import pytest
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
-
-class BasePage():
-    def __init__(self, browser, url):
-        self.browser = browser
+class BasePage:
+    def __init__(self, driver, url):
+        self.driver = driver
         self.url = url
 
     def open(self):
-        self.browser.get(self.url)
+        self.driver.get(self.url)
+
+    def find_element(self, driver, locator):
+        element = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, locator))
+        )
+        return element
--- pages/main_page.py
+++ pages/main_page.py
@@ -1,5 +1,12 @@
 import pytest
 from pages.base_page import BasePage
+from selenium.webdriver.common.by import By
+
 
 class MainPage(BasePage):
-    pass
+
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+    def go_to_login_page(self, driver):
+        login_link = driver.find_element(*self.LOGIN_LINK)
+        login_link.click()
