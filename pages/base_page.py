a07f48c1178ead91c7d190540f97fd1c98bb74ca
Dudk4 <degtyarevkolya85@gmai.com>
2025-06-08 14:36:45 +0300

    Проверка элемента на странице

--- pages/base_page.py
+++ pages/base_page.py
@@ -2,11 +2,13 @@ import pytest
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import имя_исключения
 
 class BasePage:
-    def __init__(self, driver, url):
+    def __init__(self, driver, url, timeout=10):
         self.driver = driver
         self.url = url
+        self.driver.implicitly_wait(timeout)
 
     def open(self):
         self.driver.get(self.url)
@@ -16,3 +18,10 @@ class BasePage:
             EC.presence_of_element_located((By.CSS_SELECTOR, locator))
         )
         return element
+
+    def is_element_present(self, how, what):
+        try:
+            self.driver.find_element(how, what)
+        except имя_исключения:
+            return False
+        return True
--- pages/main_page.py
+++ pages/main_page.py
@@ -6,7 +6,11 @@ from selenium.webdriver.common.by import By
 class MainPage(BasePage):
 
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_INVALID = (By.CSS_SELECTOR, "#login_link_invalid")
 
-    def go_to_login_page(self, driver):
-        login_link = driver.find_element(*self.LOGIN_LINK)
+    def go_to_login_page(self):
+        login_link = self.driver.find_element(*self.LOGIN_LINK)
         login_link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*self.LOGIN_LINK), "Login link is not presented"
