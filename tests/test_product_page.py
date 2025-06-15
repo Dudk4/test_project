6845159b6398816e7ad301e5c02db7a6777b2451
Dudk4 <degtyarevkolya85@gmai.com>
2025-06-15 21:00:39 +0300

    Задание: независимость контента, ищем баг

--- tests/test_product_page.py
+++ tests/test_product_page.py
@@ -1,7 +1,9 @@
 import pytest
 from pages.product_page import ProductPage
+from conftest import driver
 
-@pytest.mark.parametrize('promo', ["?promo=newYear"])
+@pytest.mark.parametrize('promo', ["?promo=offer0", "?promo=offer1", "?promo=offer2", "?promo=offer3",
+    "?promo=offer4", "?promo=offer5", "?promo=offer6", pytest.param("?promo=offer7", marks=pytest.mark.xfail), "?promo=offer8", "?promo=offer9"])
 def test_guest_can_add_product_to_basket(driver, promo):
     link = f"http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/{promo}"
     page = ProductPage(driver, link)
